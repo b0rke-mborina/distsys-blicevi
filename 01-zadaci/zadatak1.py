777e8eaf4ec1b6e82a99088c93cc8ca03ccd4234
b0rke-mborina <mborina@net.hr>
2022-10-17 15:52:09 +0200

    Ispravak Zadatka 1

--- 01-zadaci/zadatak1.py
+++ 01-zadaci/zadatak1.py
@@ -1,5 +1,5 @@
 def predmeti(listaPredmeta):
-	assert all(isinstance(item, str) for item in listaPredmeta)
+	assert all(isinstance(item, str) for item in listaPredmeta), "Error"
 	return {listaPredmeta.index(item):item[::-1] for item in listaPredmeta}
 
 print(predmeti(["Stol", "Stolica", "Krevet", "Fotelja"]))
