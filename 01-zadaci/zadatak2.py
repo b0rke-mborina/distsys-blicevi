98ef8edb996497f447245f58f2df9c62d2ccb448
b0rke-mborina <mborina@net.hr>
2022-10-17 16:12:33 +0200

    Ispravak Zadatka 2

--- 01-zadaci/zadatak2.py
+++ 01-zadaci/zadatak2.py
@@ -1,6 +1,5 @@
 def predmeti(firstDict, secondDict):
 	assert isinstance(firstDict, str) and isinstance(secondDict, str), "Error"
 	assert all(True if isinstance(v, list) else False for k,v in firstDict) and all(True if isinstance(v, list) else False for k,v in secondDict), "Error"
-	return {listaPredmeta.index(item):item[::-1] for item in listaPredmeta}
 
 print(predmeti(["Stol", "Stolica", "Krevet", "Fotelja"]))
