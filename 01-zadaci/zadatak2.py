86b010aec6ed523019a330a6a9a87d99fc010814
b0rke-mborina <mborina@net.hr>
2022-10-20 11:38:54 +0200

    Ispravak Zadatka 2

--- 01-zadaci/zadatak2.py
+++ 01-zadaci/zadatak2.py
@@ -1,7 +1,10 @@
-def predmeti(firstDict, secondDict):
-	assert isinstance(firstDict, str) and isinstance(secondDict, str), "Error"
-	assert all(True if isinstance(v, list) else False for k,v in firstDict) and all(True if isinstance(v, list) else False for k,v in secondDict), "Error"
-	assert "valute" and "cijena" in firstDict.keys(), "Error"
-	return []
+def conversion(firstDict, secondDict):
+	assert isinstance(firstDict, dict) and isinstance(secondDict, dict)
+	assert all([isinstance(i, list) for _,i in firstDict.items()]) and all([isinstance(i, list) for _,i in secondDict.items()])
+	assert (firstDict.get("valute") and firstDict.get("cijena")) and (secondDict.get("valute") and secondDict.get("cijena"))
+	return [(a, b) for a,d,b,c in zip(firstDict["cijena"], secondDict["cijena"], firstDict["valute"], secondDict["valute"]) if (a==d) and (b==c)]
 
-print(predmeti(["Stol", "Stolica", "Krevet", "Fotelja"]))
+print(conversion(
+	{"valute":["GBP","USD","CZK","Error"], "cijena":[8.5,7.7,0.3,10.3]},
+	{"valute":["EUR","USD","CZK","Error"], "cijena":[7.5,7.7,0.3,5.5]}
+))
