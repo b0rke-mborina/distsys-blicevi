c8949c98cb689b1977c652ae6d9171115baec07d
b0rke-mborina <mborina@net.hr>
2022-10-20 11:49:51 +0200

    Ispravak Zadatka 3

--- 01-zadaci/zadatak3.py
+++ 01-zadaci/zadatak3.py
@@ -1,8 +1,9 @@
-def predmeti(listaProizvoda):
-	assert isinstance(listaProizvoda, list) and all(isinstance(item, dict) for item in listaProizvoda), "Error"
-	return {dictValue["kategorija"] for dictinary in listaProizvoda for dictKey,dictValue in dictinary.items()}
+def proizvodi(listaProizvoda):
+	assert isinstance(listaProizvoda, list) and all([isinstance(item, dict) for item in listaProizvoda])
+	kategorije = {v for dictionary in listaProizvoda for k,v in dictionary.items() if k=="kategorija"}
+	return {kategorija:sum([proizvod["ocjena"] for proizvod in listaProizvoda if proizvod["kategorija"] == kategorija]) for dictionary in listaProizvoda for _,kategorija in dictionary.items() if kategorija in kategorije}
 
-print(predmeti([
+print(proizvodi([
 	{"naziv": "Burek", "kategorija": "pite", "ocjena": 1},
 	{"naziv": "Ramsteak", "kategorija": "steak", "ocjena": 9},
 	{"naziv": "Ribeye", "kategorija": "steak", "ocjena": 4},
